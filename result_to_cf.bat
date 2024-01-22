b54a48f2d73fc4c95d68ad06c1cc598f59b930ab
bypp <1281452@qq.com>
2024-01-23 02:19:19 +0800

    Update result_to_cf.bat

--- result_to_cf.bat
+++ result_to_cf.bat
@@ -2,11 +2,13 @@
 chcp 65001
 cls
 
-set email=
-set domain_name=
-set ip_type=
-set gloabl_api=
-set zone_id=
+for /f "tokens=1* delims==" %%a in (bat_config.txt) do (
+	if %%a == email (set email=%%b)
+	if %%a == domain_name (set domain_name=%%b)
+	if %%a == ip_type (set ip_type=%%b)
+	if %%a == zone_id (set zone_id=%%b)
+	if %%a == gloabl_api (set gloabl_api=%%b)
+)
 
 echo "开始部署优选ip到域名..."
 for /f "skip=1 delims=," %%i in (result.csv) do (
