2010be0045f302ce617305788c63e6b44a4184c4
bypp <1281452@qq.com>
2024-01-23 14:42:55 +0800

    Update clear_dns_record.bat

--- clear_dns_record.bat
+++ clear_dns_record.bat
@@ -11,16 +11,16 @@ for /f "tokens=1* delims==" %%a in (bat_config.txt) do (
 )
 
 echo "正在查询域名资料..."
-curl -s -X GET "https://api.cloudflare.com/client/v4/zones/%zone_id%/dns_records?type=%ip_type%&name=%domain_name%" -H "X-Auth-Email:%email%" -H "X-Auth-Key:%gloabl_api%" -H "Content-Type:application/json" > output.tmp
+For /F %%r in ('curl -s -X GET "https://api.cloudflare.com/client/v4/zones/%zone_id%/dns_records?type=%ip_type%&name=%domain_name%" -H "X-Auth-Email:%email%" -H "X-Auth-Key:%gloabl_api%" -H "Content-Type:application/json"') do Set dns_records=%%r
 echo "查询完毕，开始清理域名..."
-for /f %%a in ('jq ".success" output.tmp') do (
+
+for /f %%a in ('"echo %dns_records%" ^| jq .success') do (
 	if /I "%%a" == "true" (
-		for /f %%b in ('jq ".result[].id" output.tmp') do (
+		for /f %%b in ('"echo %dns_records%" ^| jq .result[].id') do (
 			curl -X DELETE "https://api.cloudflare.com/client/v4/zones/%zone_id%/dns_records/%%b" -H "X-Auth-Email:%email%" -H "X-Auth-Key:%gloabl_api%" -H "Content-Type: application/json"
 			echo.
 		)
 	) 
 )
-del output.tmp
 echo "域名清理完成"
 pause
